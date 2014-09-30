e92e88722663cddf36d7d73d2e99eee6aca48ee0
Isis Anchalee <isiswenger@gmail.com>
2014-09-30 14:16:05 -0700

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Blog::Application.routes.draw do
-  resources :articles do
+
+
+  root "welcome#index"
+
+    resources :articles do
     resources :comments
   end
-
-  root "articles#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
