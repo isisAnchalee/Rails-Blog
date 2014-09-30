6a402c572dd4bdfaaa4287e64f1ab9d3c665a402
Isis Anchalee <isiswenger@gmail.com>
2014-09-30 09:59:53 -0700

    added error handling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,4 @@
 class ArticlesController < ApplicationController
-	def new
-	end
-
-def create
-  @article = Article.new(article_params)
- 
-  @article.save
-  redirect_to @article
-end
 
  def show
   @article = Article.find(params[:id])
@@ -17,8 +8,24 @@ def index
   @articles = Article.all
 end
 
+
+def new
+  @article = Article.new
+end
+ 
+def create
+  @article = Article.new(article_params)
+ 
+  if @article.save
+    redirect_to @article
+  else
+    render 'new'
+  end
+end
+ 
 private
   def article_params
     params.require(:article).permit(:title, :text)
   end
+
 end
