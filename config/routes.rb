38f86598b0364754190710b023576f22970919f7
Isis Anchalee <isiswenger@gmail.com>
2014-09-30 13:32:49 -0700

    added comment models

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Blog::Application.routes.draw do
     resources :comments
   end
 
-  root "welcome#index"
+  root "articles#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
